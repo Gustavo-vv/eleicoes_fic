8c2707d4f363fe776a1a710cce4f675b66028dec
Gustavo-vv <gustavovieiraveloso8@gmail.com>
2026-09-05 12:01:09 -0300

    Finalizando a aula fazendo o VotoRouter

---
+++ src/dados/votos.js
@@ -0,0 +1,21 @@
+const votos = [
+    {
+        id: 1,
+        candidatoId: 1,
+        data: "2024-06-01",
+        local: "Seção 1"
+    }
+    {
+        id: 2,
+        candidatoId: 2,
+        data: "2024-06-01",
+        local: "Seção 2"
+    }
+    {
+        id: 3,
+        candidatoId: 1,
+        data: "2024-06-02",
+        local: "Seção 3"
+    }   
+]
+export {votos}
